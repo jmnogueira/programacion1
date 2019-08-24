699def3f7225eb2ae8b12280f7c8be3c685191ad
jmnogueira <52511619+jmnogueira@users.noreply.github.com>
2019-08-23 21:46:51 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*char nombre[20];
+    int edad;
+
+    printf("ingrese nombre");
+    gets(nombre);
+
+    printf("ingrese edad");
+    scanf("%d", &edad);
+
+    printf("usted se llama %s y tiene %d anios", nombre, edad);*/
+
+    int suma;
+    int numero1;
+    int numero2;
+    float promedio;
+
+    printf("ingrese un numero");
+    scanf("%d";&numero);
+
+    promedio=(float)(numero1+numero2)/2.;
+
+    printf("el promedio es %.2f", promedio);
+
+
+
+
+
+
+
+
+
+
+}
