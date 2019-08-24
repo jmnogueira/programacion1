f18c49ffa13fd2ac54594f01c61cd3ea6cdffcd7
jmnogueira <52511619+jmnogueira@users.noreply.github.com>
2019-08-23 21:50:30 -0300

    Add files via upload

---
+++ Clase1/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*int numero;
+
+    //int numero = 30;
+    //int numero2 = 12;
+    float pi= 3.14;
+    char sexo='a';
+    char nombre[20];
+
+    //char nombre[20] ="jose";
+
+
+   // printf("el nombre es: %s ",nombre);
+    // para que tome el numero hay que poner & para que lo guarde y luego la variable
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    printf("el numero ingresado es: %d ",numero);
+
+    //la mascara pàra float es %f  el .2 es para que muestre solo 2 numeros despues de la com
+
+    //printf("el valor de PI es: %.2f ",pi);
+
+   // printf(" la letra del sexo es: %c\n",sexo);
+
+    //printf("Hola Mundo!\n");
+
+
+    // %d es una mascara que se le hace al numero para que muestra el int d=decimal
+   // printf("La variable numero vale: %d y la numero2 vale: %d "
+        //   ,numero,numero2);
+
+    //printf("Hola Mundo!\n");
+    */
+    //char letra;
+   // char nombre[20];
+   // printf("ingrese nombre: ");
+
+    //scanf("%s",numbre);
+
+    // gets se usa para tomar un char desde consola, pero que sea mas que una palabra
+
+   // int edad;
+    //char nombre[20];
+
+    //printf("Ingrese su nombre");
+   // gets(nombre);
+   int numero1;
+   int numero2;
+   int suma;
+   float promedio;
+   printf("ingrese un numero");
+   scanf("%d",&numero1);
+   printf("ingrese segundo numero");
+   scanf("%d",&numero2);
+   //suma= numero1 + numero2;
+   promedio=(float)(numero1+numero2)/2;
+   printf("el promedio es %.2f",promedio);
+    //printf("ingrese su edad");
+    //scanf("%d",edad);
+
+
+
+    //system("pause");
+    return 0;
+}
+/*
+int= para numeros
+float = para numeros con coma
+char = para guardar un solo caracter
+8 bits es igual a 1 byte
+
+ver codigo aski
+
+
+*/
